068eaa344b175ae8e9400edf56979ad6c7f3e637
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-08-30 10:31:18 +0100

    creating the project and adding the dependencies

---
+++ e-learning-platform/src/main/java/com/e_learning_platform/ELearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.e_learning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningPlatformApplication.class, args);
+	}
+
+}
