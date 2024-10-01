62a173166e922a2658af55fdeec38bb35e24162d
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-10-01 16:41:15 +0100

    update

--- backEnd/src/main/java/com/backEnd/repository/OrganizationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backEnd.repository;
-
-import com.backEnd.entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-}
--- backEnd/src/main/java/com/backEnd/repository/UniversityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backEnd.repository;
-
-import com.backEnd.entity.University;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UniversityRepository extends JpaRepository<University, Long> {
-}
-
