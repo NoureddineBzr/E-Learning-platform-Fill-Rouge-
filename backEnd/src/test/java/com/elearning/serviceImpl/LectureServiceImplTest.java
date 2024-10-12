8e426319e950e8017364e30a16cd097636861ceb
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-10-13 00:45:39 +0100

    update security configs

---
+++ backEnd/src/test/java/com/elearning/serviceImpl/LectureServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.elearning.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LectureServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findByAllCourseId() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void updateCoverImage() {
+    }
+
+    @Test
+    void retrieveContent() {
+    }
+}
---
+++ backEnd/src/test/java/com/elearning/serviceImpl/PaymentServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.elearning.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentServiceImplTest {
+
+    @Test
+    void getClientSecret() {
+    }
+
+    @Test
+    void confirm() {
+    }
+
+    @Test
+    void cancel() {
+    }
+
+    @Test
+    void checkout() {
+    }
+}
